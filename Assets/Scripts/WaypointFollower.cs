9972a08299a64de4a69d3424633f2ba9c5f12094
margin25 <margin25@tamu.edu>
2023-04-20 10:12:55 -0500

    4 characters

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -1,9 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel.Design.Serialization;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
@@ -13,14 +9,27 @@ public class WaypointFollower : MonoBehaviour
 
     [SerializeField] float speed = 1f;
 
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
         {
+            int previousWaypointIndex = currentWaypointIndex;
             currentWaypointIndex++;
             if (currentWaypointIndex >= waypoints.Length)
             {
                 currentWaypointIndex = 0;
+            }
+
+            if (previousWaypointIndex != currentWaypointIndex)
+            {
+                animator.SetTrigger("WaypointReached");
             }
         }
 
